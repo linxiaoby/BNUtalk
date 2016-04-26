0e867682fc7d39769b181d276cc1efdab14ef6f2
royeson <2052132873@qq.com>
2016-04-26 21:32:49 +0800

    Update LoginActivity.java
    login service update

--- src/com/bnutalk/ui/LoginActivity.java
+++ src/com/bnutalk/ui/LoginActivity.java
@@ -8,5 +8,33 @@ public class LoginActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acitivity_login);
+		
+		forget=(TextView)findViewById(R.id.forget_key);
+		signIn=(Button)findViewById(R.id.login);
+		signUp=(TextView)findViewById(R.id.sign_up);
+
+		signIn.setOnClickListener(signInClick);
+		forget.setOnClickListener(forgetClick);
+		signUp.setOnClickListener(signUpClick);
 	}
+	View.OnClickListener forgetClick=new View.OnClickListener(){
+		@Override
+		public void onClick(View v){
+		//需补充forgetPassword活动的转换
+		}
+	};
+	View.OnClickListener signUpClick=new View.OnClickListener(){
+		@Override
+		public void onClick(View v){
+			Intent it =new Intent();
+			it.setClass(LoginActivity.this,SignUpAcitivity.class);
+			startActivity(it);
+		}
+	};
+	View.OnClickListener signInClick=new View.OnClickListener(){
+		@Override
+		public void onClick(View v){
+			//此处建立与服务器的连接
+		}
+	};
 }
